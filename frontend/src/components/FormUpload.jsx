4dfc08baa4b7f4c82baddc911c741afb9f23ce31
badi amnu <badipram23@outlook.com>
2025-08-04 12:35:30 +0700

    add loader in form upload

--- frontend/src/components/FormUpload.jsx
+++ frontend/src/components/FormUpload.jsx
@@ -1,13 +1,20 @@
-function FormUpload ({onSubmit, onFileChange}) {
-    return (
-        <form onSubmit={onSubmit}>
-            <input type="file" accept="image/*" required onChange={onFileChange} />
-            <br />
-            <button type="submit">
-                <i className="fa-solid fa-magnifying-glass"></i> Process
-            </button>
-        </form>
-    )
+function FormUpload({ onSubmit, onFileChange, loading }) {
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="file" accept="image/*" required onChange={onFileChange} />
+      <br />
+
+      {loading ? (
+        <button type="button" disabled>
+          <i className="fa-solid fa-spinner fa-spin"></i> Processing
+        </button>
+      ) : (
+        <button type="submit">
+          <i className="fa-solid fa-magnifying-glass"></i> Process
+        </button>
+      )}
+    </form>
+  );
 }
 
 export default FormUpload;
